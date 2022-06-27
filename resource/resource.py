05dc45c9e646231bcf109adc42fe3a3818da8695
Manideep1308 <manideep.sankabathula@innoart.in>
2022-06-27 18:24:14 +0530

    made error response as json

--- resource/resource.py
+++ resource/resource.py
@@ -1,9 +1,23 @@
-from flask import Flask, request
+from flask import Flask, request, json, Response
 import boto3
 from flask_cors import CORS
 
 app =Flask(__name__)
 CORS(app)
+
+app.config['PROPAGATE_EXCEPTIONS'] = True
+
+
+@app.errorhandler(Exception)
+def all_exception_handler(error):
+    res = {"error": str(error)}
+    return Response(status=500, mimetype="application/json", response=json.dumps(res))
+
+
+
+def error_401_handler(error):
+    res = {"error": "Unauthorized"}
+    return Response(status=401, mimetype="application/json", response=json.dumps(res))
 
 @app.route('/')
 def func():
